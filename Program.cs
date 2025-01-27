68009671beae6a5caf8b809c5d0c91d0b9bf8cf7
alanrichter53@gmail.com <alanrichter53@gmail.com>
2025-01-27 17:32:36 +0100

    Added modulo division and modified number input funcionality by adding string 'input' variable

--- Program.cs
+++ Program.cs
@@ -1,20 +1,22 @@
 ﻿using System.Data;
-
+string? input;
 int number1, number2;
 
 Console.WriteLine("Calculator");
 Console.WriteLine("Enter first number");
-number1 = int.Parse(Console.ReadLine());
+input = Console.ReadLine();
+number1 = int.Parse(input);
 
-Console.WriteLine("Enter first number");
-number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number");
+number2 = int.Parse(input);
 
 Console.WriteLine("What kind of operation would you like to perform?");
 Console.WriteLine("1. Addition");
 Console.WriteLine("2. Subtraction");
 Console.WriteLine("3. Multiplication");
 Console.WriteLine("4. Division");
-string choice = Console.ReadLine();
+Console.WriteLine("5. Modulo Division");
+string? choice = Console.ReadLine();
 Console.Clear();
 
 switch(choice)
@@ -30,6 +32,9 @@ switch(choice)
         break;
     case "4":
         Console.WriteLine(number1 / number2);
+        break;
+    case "5":
+        Console.WriteLine(number1 % number2);
         break;
     default:
         Console.WriteLine("There no that kind of operation.");
